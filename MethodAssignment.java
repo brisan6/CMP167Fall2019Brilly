523ff1a5979da2769ebf438716e09ebb6d4af155
brisan6 <54686564+brisan6@users.noreply.github.com>
2019-10-10 16:43:42 -0400

    Add files via upload

---
+++ MethodAssignment.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @author B Sanyer
+ *
+ **/
+
+public class MethodAssignment {
+	public static int multThree(int num1, int num2, int num3)
+	
+	return num1*num2*num3;
+	
+	}
+		
+	public static int sumVal(int num4, int num5)
+	for (int i=num4; i<=num5; i++)
+	{
+		
+	}
+		return(i);
+
+	public static int aveN(int num6)
+	for (int i )
+	}
+	
