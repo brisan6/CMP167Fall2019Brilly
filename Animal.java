28ad6bbdebdff57249dcb5baf8fa044c9a448db4
brisan6 <54686564+brisan6@users.noreply.github.com>
2019-10-15 16:27:27 -0400

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @author Brilly Sanyer
+ *
+ **/
+
+
+public class Animal {
+
+	public int numLegs;
+	private int age;
+	
+		public void bark() {
+			System.out.println("Woof Woof");
+		}
+		
+		
+		public void run() {
+			System.out.println(" ");
+			
+		
+	}
+	
+}
